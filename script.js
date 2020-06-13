9ba91277775620ecd823e9fe2038092b6cf4ba43
Vedvardhan <vedvardhan.g@gmail.com>
2020-06-14 01:21:50 +0530

    Commited

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready( function(){
     $('#button').click(() => {
-        $.get('http://numbersapi.com/' + $('#number').val(), (data, status) => {
+        $.get('https://numbersapi.com/' + $('#number').val(), (data, status) => {
             console.log(status);
             if($('#number').val() <= 100000 && $('#number').val() >= 0) $('#output').html(data); 
             else $('#output').html('Please enter a number between 0-100000')
